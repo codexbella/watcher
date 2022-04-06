ab0202aa10e595f7cad94a2d8d22e0f7dc78946b
Anna Lena Baumann <rescht@posteo.de>
2022-04-06 16:34:18 +0200

    added logout button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,35 @@
 import './App.css';
 import watcherLogo from './images/logo-light.png';
 import tmdbLogo from './images/tmdb-logo.png';
-import {Outlet} from "react-router-dom";
+import {Outlet, useNavigate} from "react-router-dom";
 import {useTranslation} from "react-i18next";
+import {useAuth} from "./auth/AuthProvider";
 
 function App() {
    const { t } = useTranslation();
+   const nav = useNavigate();
+   
+   const loginOrLogout = () => {
+      if (localStorage.getItem('jwt-token')) {
+         localStorage.setItem('jwt-token', '');
+      }
+      nav('/login')
+   }
    
    return (
       <div className="margins-left-right margin-top">
          <div className="flex row watcher">
             <img height={100} src={watcherLogo} alt=""/>
             <h1 className="color-lighter">atcher</h1>
+         </div>
+         <div className="margin-bottom">
+         <button onClick={loginOrLogout} className='no-decoration-text color-lighter large'>
+            {localStorage.getItem('jwt-token') ? t('logout') : t('login')}
+         </button>
+         {!localStorage.getItem('jwt-token') &&
+            <button onClick={() => nav('/register')} className='no-decoration-text color-lighter large'>
+               {t('register')}
+            </button>}
          </div>
          <div><Outlet/></div>
          <div className="flex row baseline gap center">
