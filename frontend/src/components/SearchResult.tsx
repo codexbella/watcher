57a7bff54a243b584e32dc28504eff4694e6a45b
Anna Lena Baumann <rescht@posteo.de>
2022-04-07 17:58:01 +0200

    added star to click to search results

--- frontend/src/components/SearchResult.tsx
+++ frontend/src/components/SearchResult.tsx
@@ -3,6 +3,7 @@ import {ShowSearchData} from "../models/ShowData";
 import {useTranslation} from "react-i18next";
 import alternateImage from '../images/alt-image.png';
 import {useNavigate} from "react-router-dom";
+import {useState} from "react";
 
 interface SearchResultProps {
    show: ShowSearchData;
@@ -11,25 +12,29 @@ interface SearchResultProps {
 export default function SearchResult(props: SearchResultProps) {
    const {t} = useTranslation();
    const nav = useNavigate();
+   const [liked, setLiked] = useState(false);
    
    return <div className="border shadow height-231 flex row margin-bottom">
       
-         <img src={props.show.posterPath ? "https://image.tmdb.org/t/p/w154" + props.show.posterPath : alternateImage} alt={props.show.name}
-              onError={(ev) => {
-                 ev.currentTarget.onerror = null;
-                 ev.currentTarget.src = alternateImage
-              }}
-         onClick={() => nav(`/search/show/${props.show.apiId}`)}
-         className='pointer'/>
+      <img src={props.show.posterPath ? "https://image.tmdb.org/t/p/w154" + props.show.posterPath : alternateImage} alt={props.show.name}
+           onError={(ev) => {
+              ev.currentTarget.onerror = null;
+              ev.currentTarget.src = alternateImage
+           }}
+           onClick={() => nav(`/search/show/${props.show.apiId}`)}
+           className='pointer'/>
       
       <div className="color-lighter flex result-details">
-         <div className="">
-               <div className="large bold small-caps pointer" onClick={() => nav(`/search/show/${props.show.apiId}`)}>
-                  {props.show.name}
-               </div>
-            
+         <div className="flex space-between">
+            <div>
+            <div className="large bold small-caps pointer" onClick={() => nav(`/search/show/${props.show.apiId}`)}>
+               {props.show.name}
+            </div>
             <div>{props.show.airDate ? new Date(props.show.airDate).getFullYear() : ''}</div>
+            </div>
+            <div className='xlarge pointer' onClick={() => setLiked(!liked)}>{liked ? <div>&#9733;</div> : <div>&#9734;</div>}</div>
          </div>
+         
          <div className="margin-top"><p className="overflow">{props.show.overview}</p></div>
          <div className="margin-top">{t('vote-average')}: {props.show.voteAverage} ({props.show.voteCount} {t('votes')})</div>
       </div>
