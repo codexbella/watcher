3100a197db8cfaa8c5274967cae8dbe6a9099ca3
Anna Lena Baumann <rescht@posteo.de>
2022-04-07 20:19:27 +0200

    reformatted code in SearchPage.tsx

--- frontend/src/SearchPage.tsx
+++ frontend/src/SearchPage.tsx
@@ -24,13 +24,13 @@ export default function SearchPage() {
       event.preventDefault();
       setSearched(true)
       if (searchTerm !== '') {
-         fetch(`${process.env.REACT_APP_BASE_URL}/search/${searchTerm}?language=${localStorage.getItem('i18nextLng')}`, {
-            method: 'GET',
-            headers: {
-               Authorization: `Bearer ${localStorage.getItem('jwt-token')}`,
-               'Content-Type': 'application/json'
-            }
-         })
+            fetch(`${process.env.REACT_APP_BASE_URL}/search/${searchTerm}?language=${localStorage.getItem('i18nextLng')}`, {
+               method: 'GET',
+               headers: {
+                  Authorization: `Bearer ${localStorage.getItem('jwt-token')}`,
+                  'Content-Type': 'application/json'
+               }
+            })
             .then(response => {
                if (response.status >= 200 && response.status < 300) {
                   return response.json();
@@ -48,7 +48,10 @@ export default function SearchPage() {
    }
    
    return <div>
-      <form onSubmit={ev => {searchForShow(ev); setShowResults([])}} className="margin-bottom">
+      <form onSubmit={ev => {
+         searchForShow(ev);
+         setShowResults([])
+      }} className="margin-bottom">
          <input className='color-lighter' type='text' placeholder={t('search-term')} value={searchTerm}
                 onChange={typed => setSearchTerm(typed.target.value)}/>
          <button type='submit'>{t('send-search-request')}</button>
@@ -62,13 +65,16 @@ export default function SearchPage() {
                      {showResults.length} {t('search-results-for-search-term')} "{searchedTerm}":
                   </div>
                }
-            
+               
                <div className='margin-bottom'>{showResults.map(item => <SearchResult show={item} key={item.apiId}/>)}
                </div>
             </div>
             :
             <div className="lds-ellipsis">
-               <div/><div/><div/><div/>
+               <div/>
+               <div/>
+               <div/>
+               <div/>
             </div>
          :
          <div/>
