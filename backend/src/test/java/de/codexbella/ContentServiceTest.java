8475119a285bd979bf0b4c2605959af01af275ea
Anna Lena Baumann <rescht@posteo.de>
2022-04-08 21:12:44 +0200

    added save verify to service test

--- backend/src/test/java/de/codexbella/ContentServiceTest.java
+++ backend/src/test/java/de/codexbella/ContentServiceTest.java
@@ -86,6 +86,9 @@ class ContentServiceTest {
 
       verify(mockApi).getForObject("https://api.themoviedb.org/3/tv/1855?api_key=xxx&language=en-US", String.class);
       verifyNoMoreInteractions(mockApi);
+
+      verify(mockShowRepo).save(any());
+      verifyNoMoreInteractions(mockShowRepo);
    }
 
    private final String searchResultOnePage = "{\n" +
