18a968701f79a55c5ed4fae6c2f880166c151437
Anna Lena Baumann <rescht@posteo.de>
2022-04-19 13:54:53 +0200

    removed unused import

--- backend/src/main/java/de/codexbella/ContentService.java
+++ backend/src/main/java/de/codexbella/ContentService.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
