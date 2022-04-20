66d4585b5d6d4334b427e52155180f01261dd19a
Anna Lena Baumann <rescht@posteo.de>
2022-04-20 10:57:09 +0200

    select for sorting shows and styling

--- frontend/src/pages/Watcherlist.tsx
+++ frontend/src/pages/Watcherlist.tsx
@@ -12,6 +12,8 @@ export default function Watcherlist() {
    const auth = useAuth()
    const [error, setError] = useState('');
    const [shows, setShows] = useState([] as Array<Show>);
+   const [sortParam, setSortParam] = useState();
+   const [showsSorted, setShowsSorted] = useState([] as Array<Show>);
    const [gotShows, setGotShows] = useState(false);
    
    const editShow = (url: string, index: number) => {
@@ -88,14 +90,33 @@ export default function Watcherlist() {
          getAllShows();
    }, [getAllShows])
    
+   const sortShows = () => {
+      setShowsSorted(shows.sort())
+   }
+   
    return <div>
       <div className='color-lighter margin-bottom-15px larger'>{t('hello')} {auth.username ?? t('there')}!</div>
       
       {gotShows ?
          <div>
-            <div className="large color-light margin-bottom-15px">
+            <div className='flex justify-space-between color-light '>
+            <div className='large margin-bottom-15px'>
                {t('you-have')} {shows.length} {t('shows-in-your-list')}:
             </div>
+            <form onSubmit={getAllShows}>
+               <label htmlFor='sort-by' className='large'>{t('sort-by')}: </label>
+               <select onSubmit={getAllShows} id='sort-by' className='background-dark medium color-lighter border-dark'>
+                  <option value='notSeen'>{t('not-seen')}</option>
+                  <option value='rating'>{t('own-rating')}</option>
+                  <option value='vote'>{t('vote-average')}</option>
+                  <option value='voteCount'>{t('vote-count')}</option>
+                  <option value='inProduction'>{t('in-production')}</option>
+                  <option value='airDate'>{t('airdate')}</option>
+                  <option value='name'>{t('name')}</option>
+                  <option value='added' selected>{t('added')}</option>
+               </select>
+            </form>
+         </div>
             <div className='flex wrap gap-20px margin-bottom-15px'>
                {shows.map((item, index) =>
                   <ShowComponent show={item} key={item.id} index={index} onChange={getAllShows} onRating={determineRateUrl}
